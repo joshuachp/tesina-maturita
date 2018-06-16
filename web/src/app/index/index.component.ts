27e0e4cadc935cf7345b81eb41ff9053b1465f34
Joshua Chapman <john.chy99@gmail.com>
2018-06-16 14:49:09 +0200

    Try images

--- web/src/app/index/index.component.ts
+++ web/src/app/index/index.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class IndexComponent {
     cards = [
-        { title: 'Linear Regression', cols: 2, rows: 1, link: '/linear', content: 'Simple linear regression example.' },
+        { title: 'Linear Regression', cols: 2, rows: 1, link: '/linear', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Linear_regression.svg/1200px-Linear_regression.svg.png' },
         { title: 'MNIST', cols: 1, rows: 1, link: '/mnist', content: 'Implementation of a hand-written digit reconizer.' },
         { title: 'Card 3', cols: 1, rows: 2, link: '', content: '' },
         { title: 'Card 4', cols: 1, rows: 1, link: '', content: '' }
