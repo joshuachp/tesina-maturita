9104e531bbd97b55a259c5aff7490b88fa8fe8e8
Joshua Chapman <john.chy99@gmail.com>
2018-06-18 09:45:31 +0200

    Correct design

--- web/src/app/index/index.component.ts
+++ web/src/app/index/index.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class IndexComponent {
     cards = [
-        { title: 'Linear Regression', cols: 2, rows: 1, link: '/linear', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Linear_regression.svg/1200px-Linear_regression.svg.png' },
-        { title: 'MNIST', cols: 1, rows: 1, link: '/mnist', content: 'Implementation of a hand-written digit reconizer.' },
-        { title: 'Card 3', cols: 1, rows: 2, link: '', content: '' },
+        { title: 'Introduction', cols: 1, rows: 1, link: '', content: 'Introduzione ai vari tipi di Intelligenza Artificiale (IA) e in particolare agli algoritmi di Machine Learning per le Neural Networks.' },
+        { title: 'Linear Regression', cols: 1, rows: 2, link: '/linear', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Linear_regression.svg/1200px-Linear_regression.svg.png' },
+        { title: 'MNIST', cols: 1, rows: 2, link: '/mnist', image: 'https://dmtyylqvwgyxw.cloudfront.net/instances/132/uploads/images/custom_image/image/1574/normal_687474703a2f2f692e7974696d672e636f6d2f76692f3051493378675875422d512f687164656661756c742e6a7067.jpg?v=1507808945' },
+        { title: 'Card 4', cols: 1, rows: 2, link: '', content: '' },
         { title: 'Card 4', cols: 1, rows: 1, link: '', content: '' }
     ];
 }
